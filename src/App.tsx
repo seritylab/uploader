dccb181fe107c071cb08cc736e992819318f6e8a
seritylab <seritylab@gmail.com>
2022-10-02 17:05:43 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-slate-800 h-screen flex justify-center">
+      <p className="text-3xl font-bold">
+        Serity Uploader
+      </p>
+    </div>
+  )
+}
+
+export default App
