0babae1f88b6e74ceb1d2d8a8ce5b36cc4afd08e
seritylab <seritylab@gmail.com>
2022-10-02 17:38:49 -0600

    Attemp to add dark mode with tailwindscss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
+import Navbar from "./components/Navbar"
 
 function App() {
 
+  const [darkToggle, setDarkToggle] = useState(false);
+
   return (
-    <div className="bg-slate-800 h-screen flex justify-center">
+    <div className="bg-white dark:bg-slate-800 h-screen flex justify-center">
+      <Navbar/>
       <p className="text-3xl font-bold">
         Serity Uploader
       </p>
+
+      <input type="checkbox" onClick={() => setDarkToggle(!darkToggle)} />
+
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return (
+        <div>   
+            <p className="text-3xl font-bold">
+        Serity Uploader
+      </p> 
+      </div>
+    )
+}
+
+export default Navbar;
